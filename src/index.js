35868d127651ba7a7e6a1cddb44656c4afb19c0f
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-08-11 14:12:50 -0300

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//El primer paso para utilizar React es importar la biblioteca, esta se encuentra en el package.json
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const rootElement = document.getElementById('root')
+// ReactDOM.createRoot(rootElement)
+//Equivake a..
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<h1>Hello word</h1>)
+/*COMPONENTES: son porciones de codigo en las que podemos dividir una pagina para asi poder reutilizar el codigo*/
+/*Son funciones que devuelven una porcion de interfaz*/
+/*Los elementos siempre deben tener un elemento padre para ser llamados*/
+function Greeting() {
+  // return <div>
+  //         <h1>Primer componente en react </h1>
+  //         <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+  //         </div>
+  const user = {
+    primerNombre: "Santiago",
+    apellidoQuiroz: "Quiroz",
+  };
+
+  return <h1>{JSON.stringify(user)}</h1>;
+}
+/*El componente Greeting puede ser utilizado dentro del programa como una etiqueta html que llama al componente js*/
+/*Estas etiquetas pueden ser de autocerrado */
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Greeting />
+    <Greeting></Greeting>
+    <Greeting></Greeting>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*FRAGMENT: son contenedores vacios que sirven para cumplir con la regla de react de tener un contenedor padre sin utilizar alguna etiqueta especifica*/
